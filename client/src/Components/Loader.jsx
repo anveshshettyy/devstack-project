bfe86f54f7a4b98947463f583770843d18ea4a2c
Anvesh Shetty <shettyanvesh86@gmail.com>
2026-07-08 20:10:52 +0530

    Enhanced Loader

--- client/src/Components/Loader.jsx
+++ client/src/Components/Loader.jsx
@@ -5,7 +5,7 @@ export default function Loader() {
     <div className="flex justify-center items-center h-screen">
       <div className="relative flex items-center justify-center">
         <div className="w-16 h-16 rounded-full border-4 border-gray-300 border-t-black animate-spin"></div>
-        <span className="absolute text-xs text-gray-700 font-semibold animate-pulse">Loading...</span>
+        <span className="absolute text-xs text-gray-700 font-semibold animate-pulse"></span>
       </div>
     </div>
   );
